7b885761dc9f5d374e9f764c2ecc64a54a300e29
YueZhang-studyuse <brynn.yuezhang@gmail.com>
2025-02-05 18:38:49 +1100

    optimise ta by reducing edges

--- default_planner/scheduler.cpp
+++ default_planner/scheduler.cpp
@@ -95,103 +95,40 @@ void schedule_plan_raw(int time_limit, std::vector<int> & proposed_schedule,  Sh
 
 void schedule_plan(int time_limit, std::vector<int> & proposed_schedule,  SharedEnvironment* env, std::vector<Int4> background_flow)
 {
-    // // //use at most half of time_limit to compute schedule, -10 for timing error tolerance
-    // //so that the remainning time are left for path planner
-    // TimePoint endtime = std::chrono::steady_clock::now() + std::chrono::milliseconds(time_limit);
-    // // cout<<"schedule plan limit" << time_limit <<endl;
 
-    // the default scheduler keep track of all the free agents and unassigned (=free) tasks across timesteps
-    // free_agents.insert(env->new_freeagents.begin(), env->new_freeagents.end());
-    // free_tasks.insert(env->new_tasks.begin(), env->new_tasks.end());
-    // proposed_schedule.resize(env->num_of_agents, -1); //default no schedule
-
-
-    // int i_task, min_task_i, min_task_makespan, dist, c_loc, count;
-    // clock_t start = clock();
-
-    // for (int i = 0; i < env->num_of_agents && std::chrono::steady_clock::now() < endtime; i++)
-    // {
-        
-    //     if (env->curr_task_schedule[i] == -1)
-    //     {
-            
-    //         min_task_i = -1;
-    //         min_task_makespan = INT_MAX;
-    //         count = 0;
-    //         for (i_task=0 ; i_task < env->task_pool.size() && std::chrono::steady_clock::now() < endtime ;i_task++)
-    //         {                
-
-    //             if (env->task_pool[i_task].agent_assigned != -1)
-    //                 continue;
-    //             dist = 0;
-    //             c_loc = env->curr_states.at(i).location;
-    //             for (int loc : env->task_pool[i_task].locations){
-    //                 dist += DefaultPlanner::get_h(env, c_loc, loc);
-    //                 c_loc = loc;
-    //             }
-    //             if (dist < min_task_makespan){
-    //                 min_task_i = i_task;
-    //                 min_task_makespan = dist;
-    //             }
-    //             count++;            
-    //         }
-
-
-    //         if (min_task_i != -1){
-    //             proposed_schedule[i] = env->task_pool[min_task_i].task_id;
-    //             env->task_pool[min_task_i].agent_assigned = i;
-    //         }
-    //         else{
-    //             proposed_schedule[i] = -1;
-    //         }
-            
-
-    //     }
-    //     else
-    //     {
-    //         proposed_schedule[i] = env->curr_task_schedule[i];
-    //     }
-    // }
-    // cout << ((float)(clock() - start))/CLOCKS_PER_SEC <<endl;
+    int maximum_edges = 100;
 
     proposed_schedule.resize(env->num_of_agents, -1);
 
-    unordered_set<int>opened_agents;
-    // unordered_set<int>opened_tasks;
-    vector<int>agent_ids;
-    vector<int>task_ids;
-
-    //pre check the opened tasks.
-    // for (int i_task=0 ; i_task < env->task_pool.size() ;i_task++)
-    for (auto task: env->task_pool)
-    {
-        if (task.second.idx_next_loc > 0) //task opened
-        {
-            opened_agents.insert(task.second.agent_assigned);
-            // opened_tasks.insert(i_task);
-            proposed_schedule[task.second.agent_assigned] = task.first;
-        }
-        else
-        {
-            task_ids.push_back(task.first);
-        }
-    }
+    vector<int>flexible_agent_ids(env->new_freeagents); //storing the agents not doing a opened task
+    vector<int>flexible_task_ids(env->new_tasks); //storing the tasks we consider to swap/assign
 
     for (int i = 0; i < env->num_of_agents; i++)
     {
-        if (opened_agents.find(i) == opened_agents.end())
+        int t_id = env->curr_task_schedule[i];
+        if (t_id >= 0 && env->task_pool[t_id].idx_next_loc == 0) //task assigned but not opened
         {
-            agent_ids.push_back(i);
+            flexible_agent_ids.push_back(i);
+            flexible_task_ids.push_back(t_id);
+        }
+        else if (t_id >= 0) //task opened
+        {
+            proposed_schedule[i] = t_id;
         }
     }
 
-    cout<<"num of background agents: "<<opened_agents.size()<<endl;
+    //prepare for matching
 
-    int num_workers = agent_ids.size();
-    int num_tasks = task_ids.size();
+    cout<<"num of flexible agents: "<<flexible_agent_ids.size()<<endl;
+    cout<<"num of flexible tasks: "<<flexible_task_ids.size()<<endl;
 
+    int num_workers = flexible_agent_ids.size();
+    int num_tasks = flexible_task_ids.size();
 
+    // maximum_edges = num_workers;
 
+    if (maximum_edges > num_tasks)
+        maximum_edges = num_tasks;
 
     //Create an environment
     GRBEnv grb_env = GRBEnv();
@@ -206,108 +143,115 @@ void schedule_plan(int time_limit, std::vector<int> & proposed_schedule,  Shared
 
     GRBLinExpr objective = 0;
 
-    //naive code for dijkstra using traffic distance
-    vector<unordered_map<int,int>> task_heuristics;
-    task_heuristics.resize(num_tasks);
+    //computing heuristics
+    vector<unordered_map<int,int>> agent_task_heuristic;
+    agent_task_heuristic.resize(env->num_of_agents);
     std::deque<HNode> open;
     std::unordered_map<int,HNode*> all_nodes;
-    //add locations
-    unordered_set<int> dij_goals;
     unordered_set<int> closed;
-    if (env->curr_timestep > 0)
+    unordered_map<int,list<int>> task_loc_ids;
+    int goal_reach_cnt;
+    for (int id: flexible_task_ids)
     {
-        for (int j = 0; j < num_tasks; j++)
+        task_loc_ids[env->task_pool[id].locations[0]].push_back(id);
+    }
+
+    for (int id: flexible_agent_ids)
+    {
+        open.clear();
+        closed.clear();
+        goal_reach_cnt = 0;
+        int goal_location = env->curr_states[id].location;
+        HNode root(goal_location,0, 0);
+        open.push_back(root);
+        closed.insert(goal_location);
+
+        std::vector<int> neighbors;
+        int  diff, d, cost, op_flow, total_cross, all_vertex_flow,vertex_flow, depth,p_diff, p_d;
+        int next_d1, next_d2, next_d1_loc, next_d2_loc;
+        int temp_op, temp_vertex;
+
+        while (!open.empty())
         {
-            dij_goals.clear();
-            dij_goals.insert(env->task_pool[task_ids[j]].locations[1]);
-            for (int i = 0; i < num_workers; i++)
+            HNode curr = open.front();
+            open.pop_front();
+            closed.insert(curr.location);
+            if (task_loc_ids.find(curr.location)!= task_loc_ids.end())
             {
-                dij_goals.insert(env->curr_states.at(agent_ids[i]).location);
-            }
-
-            open.clear();
-            closed.clear();
-            int goal_location = env->task_pool[task_ids[j]].locations[0];
-            HNode root(goal_location,0, 0);
-            open.push_back(root);
-            closed.insert(goal_location);
-
-            std::vector<int> neighbors;
-            int  diff, d, cost, op_flow, total_cross, all_vertex_flow,vertex_flow, depth,p_diff, p_d;
-            int next_d1, next_d2, next_d1_loc, next_d2_loc;
-            int temp_op, temp_vertex;
-            while (!open.empty())
-            {
-                HNode curr = open.front();
-                open.pop_front();
-                closed.insert(curr.location);
-                if (dij_goals.find(curr.location) != dij_goals.end())
+                for (int t_id: task_loc_ids[curr.location])
                 {
-                    task_heuristics[j][curr.location] = curr.value;
-                    dij_goals.erase(curr.location);
-                    //cout<<"find goal"<<curr.location<<" "<<dij_goals.size()<<endl;
-                }
-                if (dij_goals.empty())
-                    break;
-                
-                neighbors = global_neighbors.at(curr.location);
-                
-                for (int next : neighbors)
-                {
-                    if (closed.find(next) != closed.end())
-                        continue;
-                    
-                    cost = curr.value + 1;
-                    op_flow = 0;
-                    all_vertex_flow = 0;
-                    diff = curr.location-next;
-                    d = get_d(diff,env);
-                    temp_op = ( (background_flow[curr.location].d[d]+1) * background_flow[next].d[(d+2)%4]);
-                    temp_vertex = 1;
-                    for (int j=0; j<4; j++)
+                    agent_task_heuristic[id][t_id] = curr.value;
+                    goal_reach_cnt++;
+                    if (env->task_pool[t_id].agent_assigned < 0 && env->curr_task_schedule[id] < 0) //no assignment yet
                     {
-                        temp_vertex += background_flow[next].d[j];                
+                        //set an assignment greedily
+                        env->curr_task_schedule[id] = t_id;
+                        env->task_pool[t_id].agent_assigned = id;
                     }
-                    op_flow += temp_op;
-                    all_vertex_flow+= (temp_vertex-1) /2;
-
-                    cost = cost + op_flow + all_vertex_flow;
-
-                    if (all_nodes.find(next) != all_nodes.end())
-                    {
-                        HNode* old = all_nodes[next];
-                        if (cost < old->value)
-                        {
-                            old->value = cost;
-                        }
-                    }
-                    else
-                    {
-                        HNode next_node(next,0, cost);
-                        open.push_back(next_node);
-                        all_nodes[next] = &next_node;
-                    }
-                    
                 }
             }
-            all_nodes.clear();
+
+            if (env->curr_task_schedule[id] >= 0 && goal_reach_cnt >= maximum_edges && agent_task_heuristic[id].find(env->curr_task_schedule[id]) != agent_task_heuristic[id].end())
+                break;
+            
+            neighbors = global_neighbors.at(curr.location);
+            
+            for (int next : neighbors)
+            {
+                if (closed.find(next) != closed.end())
+                    continue;
+                
+                cost = curr.value + 1;
+                op_flow = 0;
+                all_vertex_flow = 0;
+                diff = curr.location-next;
+                d = get_d(diff,env);
+                temp_op = ( (background_flow[curr.location].d[d]+1) * background_flow[next].d[(d+2)%4]);
+                temp_vertex = 1;
+                for (int j=0; j<4; j++)
+                {
+                    temp_vertex += background_flow[next].d[j];                
+                }
+                op_flow += temp_op;
+                all_vertex_flow+= (temp_vertex-1) /2;
+
+                cost = cost + op_flow + all_vertex_flow;
+
+                if (all_nodes.find(next) != all_nodes.end())
+                {
+                    HNode* old = all_nodes[next];
+                    if (cost < old->value)
+                    {
+                        old->value = cost;
+                    }
+                }
+                else
+                {
+                    HNode next_node(next,0, cost);
+                    open.push_back(next_node);
+                    all_nodes[next] = &next_node;
+                }
+                
+            }
         }
+        all_nodes.clear();
     }
 
     for (int i = 0; i < num_workers; i++)
     {
-        int start = env->curr_states.at(agent_ids[i]).location;
         for (int j = 0; j < num_tasks; j++)
         {
-            //we assume pick_up + delivery
-            int goal = env->task_pool[task_ids[j]].locations[0];
-            int goal2 = env->task_pool[task_ids[j]].locations[1];
-            if (env->curr_timestep == 0)
-                cost[i][j] = DefaultPlanner::get_h(env, start, goal) + DefaultPlanner::get_h(env, goal, goal2);
-            else
-                cost[i][j] = task_heuristics[j][start]+task_heuristics[j][goal2];
+            if (agent_task_heuristic[flexible_agent_ids[i]].find(flexible_task_ids[j]) == agent_task_heuristic[flexible_agent_ids[i]].end())
+                continue;
+            cost[i][j] = agent_task_heuristic[flexible_agent_ids[i]][flexible_task_ids[j]];
             x[i][j] = model.addVar(0.0, 1.0, cost[i][j], GRB_BINARY, "x_" + std::to_string(i) + "_" + std::to_string(j));
             objective += cost[i][j] * x[i][j];
+
+            // //warm start with initial solution
+            if (env->curr_task_schedule[flexible_agent_ids[i]] == flexible_task_ids[j])
+                x[i][j].set(GRB_DoubleAttr_Start, 1.0);
+            else    
+                x[i][j].set(GRB_DoubleAttr_Start, 0.0);
         }
     }
 
@@ -318,6 +262,8 @@ void schedule_plan(int time_limit, std::vector<int> & proposed_schedule,  Shared
         GRBLinExpr worker_sum = 0;
         for (int j = 0; j < num_tasks; ++j) 
         {
+            if (agent_task_heuristic[flexible_agent_ids[i]].find(flexible_task_ids[j]) == agent_task_heuristic[flexible_agent_ids[i]].end())
+                continue;
             worker_sum += x[i][j];
         }
         model.addConstr(worker_sum == 1, "worker_" + std::to_string(i));
@@ -329,6 +275,8 @@ void schedule_plan(int time_limit, std::vector<int> & proposed_schedule,  Shared
         GRBLinExpr task_sum = 0;
         for (int i = 0; i < num_workers; ++i)
         {
+            if (agent_task_heuristic[flexible_agent_ids[i]].find(flexible_task_ids[j]) == agent_task_heuristic[flexible_agent_ids[i]].end())
+                continue;
             task_sum += x[i][j];
         }
         model.addConstr(task_sum <= 1, "task_" + std::to_string(j));
@@ -344,12 +292,15 @@ void schedule_plan(int time_limit, std::vector<int> & proposed_schedule,  Shared
         {
             for (int j = 0; j < num_tasks; ++j) 
             {
+                if (agent_task_heuristic[flexible_agent_ids[i]].find(flexible_task_ids[j]) == agent_task_heuristic[flexible_agent_ids[i]].end())
+                    continue; // Skip invalid assignments
+                    
                 if (x[i][j].get(GRB_DoubleAttr_X) > 0.5) 
                 {  // Check if x[i][j] is 1
                     std::cout << "Worker " << i << " assigned to Task " << j
                                 << " with cost " << cost[i][j] << std::endl;
-                    proposed_schedule[agent_ids[i]] = env->task_pool[task_ids[j]].task_id;
-                    env->task_pool[task_ids[j]].agent_assigned = agent_ids[i];
+                    proposed_schedule[flexible_agent_ids[i]] = env->task_pool[flexible_task_ids[j]].task_id;
+                    env->task_pool[flexible_task_ids[j]].agent_assigned = flexible_agent_ids[i];
                 }
             }
         }
