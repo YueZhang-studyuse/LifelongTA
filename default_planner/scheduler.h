5aa21dafa3e2c25bbce6c3bce62a776d44d831b7
YueZhang-studyuse <brynn.yuezhang@gmail.com>
2025-02-05 15:16:56 +1100

    bug fixing

--- default_planner/scheduler.h
+++ default_planner/scheduler.h
@@ -11,6 +11,7 @@ namespace DefaultPlanner{
 void schedule_initialize(int preprocess_time_limit, SharedEnvironment* env);
 
 void schedule_plan(int time_limit, std::vector<int> & proposed_schedule,  SharedEnvironment* env, std::vector<Int4> background_flow);
+void schedule_plan_raw(int time_limit, std::vector<int> & proposed_schedule,  SharedEnvironment* env);
 
 }
 
