e6b893cb3f3b4c6894fa3666545a7041d17e4d9e
YueZhang-studyuse <brynn.yuezhang@gmail.com>
2025-07-01 22:55:19 +1000

    more choice for scheduler

--- src/TaskScheduler.cpp
+++ src/TaskScheduler.cpp
@@ -35,25 +35,26 @@ void TaskScheduler::plan(int time_limit, std::vector<int> & proposed_schedule)
     //-SCHEDULER_TIMELIMIT_TOLERANCE for timing error tolerance
     int limit = time_limit/2 - DefaultPlanner::SCHEDULER_TIMELIMIT_TOLERANCE;
 
-    if (!use_traffic)
+    if (solver == 1)
+    {
+        DefaultPlanner::schedule_plan_flow(limit, proposed_schedule, env, background_flow, use_traffic, new_only);
+    }
+    else if (solver == 2)
+    {
+        DefaultPlanner::schedule_plan_flow_hist(limit, proposed_schedule, env, env->past_waitings, new_only);
+    }
+    else if (solver == 3)
+    {
+        DefaultPlanner::schedule_plan_matching(limit, proposed_schedule, env, background_flow, use_traffic, new_only, max_matching_edges);
+    }
+    else  if (solver == 4 && !use_traffic)
     {
         DefaultPlanner::schedule_plan_h(limit, proposed_schedule, env, new_only);
     }
     else
     {
-        if (solver == 1)
-        {
-            DefaultPlanner::schedule_plan_matching(limit, proposed_schedule, env, background_flow, use_traffic, new_only, max_matching_edges);
-        }
-        else if (solver == 2)
-        {
-            DefaultPlanner::schedule_plan_flow(limit, proposed_schedule, env, background_flow, use_traffic, new_only);
-        }
-        else
-        {
-            std::cerr << "Invalid solver type. Please choose either 1 (matching) or 2 (flow)." << std::endl;
-            exit(1);
-        }
+        std::cerr << "Invalid solver type. Please choose either 1 (matching) or 2 (flow)." << std::endl;
+        exit(1);
     }
 }
 
